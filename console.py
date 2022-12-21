e69c274ea834804648bbba716cedf630f4cae4d3
Fraser Jubb <fraser.jubb@gmail.com>
2022-12-21 13:12:19 +0000

    Added more 'dummy data' to console. Everything working.

--- console.py
+++ console.py
@@ -11,11 +11,17 @@ owner_repository.delete_all()
 vet_repository.delete_all()
 
 # owners:
-owner_1 = Owner('Fraser', '07932773255')
+owner_1 = Owner('Fraser Jubb', '07932773255')
 owner_repository.save(owner_1)
 
 owner_2 = Owner('Satan', '66666666666')
 owner_repository.save(owner_2)
+
+owner_3 = Owner('John Wick', '987460385024')
+owner_repository.save(owner_3)
+
+owner_4 = Owner('Master Splinter', '892782404818')
+owner_repository.save(owner_4)
 
 
 # vets:
@@ -25,15 +31,28 @@ vet_repository.save(vet_1)
 vet_2 = Vet('Dr R Williams')
 vet_repository.save(vet_2)
 
+
 #pets:
-pet_1 = Pet('Daisy', '01.01.2011', 'Dog', '', owner_1, vet_2)
+pet_1 = Pet('Daisy', '01.01.2017', 'Dog', '', owner_1, vet_2)
 pet_repository.save(pet_1)
 
-pet_2 = Pet('Garfield', '03.11.2005', 'Cat', 'Obesity', owner_1, vet_1)
+pet_2 = Pet('Garfield', '03.11.2005', 'Cat', 'Obesity issues likely due to excessive lasagne consumption.', owner_2, vet_1)
 pet_repository.save(pet_2)
  
-pet_3 = Pet('Cerberus', '06.06.2006', 'Dog', 'Has 3 heads', owner_2, vet_1)
+pet_3 = Pet('Cerberus', '06.06.2006', 'Dog', 'Has three heads and highly aggressive.', owner_2, vet_1)
 pet_repository.save(pet_3)
 
-pet_4 = Pet('WHY', '06.06.2006', 'Dog','', owner_1, vet_1)
+pet_4 = Pet('Smudge', '06.12.2020', 'Rabbit','', owner_3, vet_1)
 pet_repository.save(pet_4)
+
+pet_5 = Pet('Leonardo', '06.12.2020', 'Turtle','', owner_4, vet_2)
+pet_repository.save(pet_5)
+
+pet_6 = Pet('Raphael', '06.12.2020', 'Turtle','', owner_4, vet_2)
+pet_repository.save(pet_6)
+
+pet_7 = Pet('Donatello', '06.12.2020', 'Turtle','', owner_4, vet_2)
+pet_repository.save(pet_7)
+
+pet_8 = Pet('Michelangelo', '06.12.2020', 'Turtle','', owner_4, vet_2)
+pet_repository.save(pet_8)
